b54783b8b1de44b978c823da097f70dfb6776ed9
Ruym?n Reyes Castro <rreyes@ull.es>
2010-08-10 14:03:53 +0100

    - Script to update pycparser patch

--- utils/pycparser/update_patch.sh
+++ utils/pycparser/update_patch.sh
@@ -1,11 +1,23 @@
 
+if [ "$#" -ne 1 ];then
+  echo " Incorrect option. Use \"apply\" or \"update\""
+  exit 
+fi
+
+APPLY=$1
+
 FILES="_ast_gen.py _c_ast.yaml c_lexer.py c_parser.py"
 for file in $FILES; do
 	PATCHFILE=`echo -n "$file" | cut -f 1 -d . | tr -d "\n" ; echo ".patch"`
-	svn diff pycparser/$file > $PATCHFILE
+        if [ "$APPLY" = "apply" ]; then
+		patch -p 0 pycparser/$file < $PATCHFILE
+        else 
+		hg diff pycparser/$file > $PATCHFILE
+	fi
         echo " Updating $file"
 done
 
-cp *.patch ../llcomp/pycparser/
-cp z_test.py ../llcomp/pycparser/
-
+if [ "$APPLY" != "apply" ]; then
+	cp *.patch ../llcomp/utils/pycparser/
+	cp z_test.py ../llcomp/utils/pycparser/
+fi
